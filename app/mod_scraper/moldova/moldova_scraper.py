8c96e8c7cf16ff58d3488c417a101749876f8042
Vullkan92 <vullkan.halili@riinvest.net>
2015-09-02 09:23:24 +0200

    Scraped and posted people data from Moldova's parliament.

--- app/mod_scraper/moldova/moldova_scraper.py
+++ app/mod_scraper/moldova/moldova_scraper.py
@@ -2,11 +2,11 @@
 from pymongo import MongoClient
 from app.mod_scraper import scraper
 import re
-import requests
-import pprint
 
 client = MongoClient()
 db = client.parlament_md
+scrape = scraper.Scraper()
+
 
 class MoldovaScraper():
 
@@ -22,67 +22,79 @@ class MoldovaScraper():
     #     else:
     #         return dt.strftime('%Y-%m-%d')
 
-    def scrape_mp_bio_data(self):
-        db.mps_list.remove({})
+    def guess_gender(self, first_name):
+        if first_name[-1:] == "a":
+            return "female"
+        else:
+            return "male"
 
-        print "\nScraping members (people) of Moldova's parliament..."
+    def build_json_doc(self, identifier, full_name, first_name, last_name, url, image_url, gender):
+        json_doc = {
+            "identifiers": [{
+                "identifier": identifier,
+                "scheme": "parlament.md"
+            }],
+            "gender": gender,
+            "name": full_name,
+            "given_name": first_name,
+            "family_name": last_name,
+            "sources": [{
+                "note": "pagină web",
+                "url": url
+            }],
+            "image": image_url,
+            "sort_name": last_name + ", " + first_name
+        }
+        return json_doc
 
+    def mps_list(self):
+        deputy_list = []
         deputy_list_url = "http://www.parlament.md/StructuraParlamentului/Deputies/tabid/87/language/ro-RO/Default.aspx"
-
-        scrape = scraper.Scraper()
-
         soup = scrape.download_html_file(deputy_list_url)
-
-        counter = 0
-
-        each = soup.findAll("table", {"cellspacing": "4"})[0].findAll('tr')
+        each = soup.find("div", {"class": "allTitle"}).find("table", {"cellspacing": "4"}).findAll('tr')
 
         for tr in each:
             name = tr.find("td").find("img").get("alt")
             link = tr.find("td", {"valign": "top"}).find("a").get("href")
-            fraction_link = tr.find("td", {"valign": "top"}).find("a", {"class": "FractionLink"}).get("href")
-            fraction = tr.find("td", {"valign": "top"}).find("a", {"class": "FractionLink"}).next
+            # fraction_link = tr.find("td", {"valign": "top"}).find("a", {"class": "FractionLink"}).get("href")
+            # fraction = tr.find("td", {"valign": "top"}).find("a", {"class": "FractionLink"}).next
             image_url = "http://www.parlament.md" + tr.find("td").find("img").get("src")
-
-            print "<<<<<<<<<<<<<<<<<<<<<<<"
-            print "name: " + name
-            print "fraction: " + fraction
-            print "fraction_link: " + fraction_link
-            print "image_url: " + image_url
-            print "link: " + link
-
-            if "id/" in link:
-                print link.index('Id/')
-                print link.index('/la')
-                person_id_url = link[:link.index('/la')]
-                person_id = person_id_url[link.index('/Id/'):]
-                print "PERSON_ID: " + person_id.replace('/Id/', '')
-            print "identifier: " + person_id_url[:4].replace('/', "")
+            names = name.split(' ')
+            first_name = names[1]
+            last_name = names[0]
+            sort_name = names[0] + ", " + names[1]
+            name_ordered = first_name + " " + last_name
+            gender = self.guess_gender(first_name)
+            index_start = link.index('Id/')
+            index_end = link.index('/la')
+            member_id = link[index_start + 3:index_end]
             soup_deputy = scrape.download_html_file(link)
             membership = soup_deputy.find("span", {"id": "dnn_ctr476_ViewDeputat_lblPosition"})
-            birth_date = soup_deputy.find("fieldset", {"id": "dnn_ctr476_ViewDeputat_fsCurriculumVitae"}).find("span")
-            print birth_date
-            if membership.next[:1] != "P":
-                labels = soup_deputy.findAll("td", {"class": "SubHead"})
-                committe = soup_deputy.find("a", {"id": "dnn_ctr476_ViewDeputat_hlCommission"})
-                party = soup_deputy.find("a", {"id": "dnn_ctr476_ViewDeputat_hlFraction"})
-                label_committe = ""
-                label_party = ""
-                for label in labels:
-                    if label.has_attr('style'):
-                        label_committe = label.get_text()
-                    else:
-                        label_party = label.get_text()
-                print "%s %s" % (label_party.strip().replace(' ', "_").lower(), party.next)
-                print "%s %s" % (label_committe.strip().replace(' ', "_").lower(), committe.next)
-                print ">>>>>>>>>>>>>>>>>>>>>>>"
-            else:
-                print "member: " + membership.next
-                print ">>>>>>>>>>>>>>>>>>>>>>>"
+            deputy_json = {
+                "membership": membership.get_text(),
+                "identifier": member_id,
+                "name": name_ordered,
+                "sort_name": sort_name,
+                "gender": gender,
+                "url": link,
+                "image_url": image_url,
+                "given_name": first_name,
+                "family_name": last_name,
+            }
+            deputy_list.append(deputy_json)
+        return deputy_list
 
-            counter += 1
-
-        print "Scraping compete!\nScraped %s members " % str(counter)
-
-    def scrape_organization(self):
-        print "scraping Moldova Votes data"
+    def scrape_mp_bio_data(self):
+        mps_list = self.mps_list()
+        members = []
+        print "\n\tScraping people data from Moldova's parliament..."
+        print "\tThis may take a few minutes..."
+        for member in mps_list:
+            # identifier, full_name, first_name, last_name, url, image_url, gender
+            member_json = self.build_json_doc(member['identifier'], member['name'], member['given_name'],
+                                              member['family_name'], member['url'], member['image_url'],
+                                              member['gender'])
+            members.append(member_json)
+        print "\n\tScraping completed! \n\tScraped " + str(len(members)) + " members"
+        return members
+        # print "Scraped %s members" % str(counter)
