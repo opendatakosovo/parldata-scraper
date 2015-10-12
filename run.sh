941ddc94f4f002154c74ad808fd0758dbc6b586f
Vullkan92 <vullkan.halili@riinvest.net>
2015-10-12 14:37:45 +0200

    minor changes

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 source ./venv/bin/activate
 if [ "$#" -ne 4 ]; then
-    echo "Invalid number of parameters."
+    echo "Invalid number of parameters.."
 else
     python run.py --countries $1 --people $2 --votes $3 --time_out_seconds $4
 fi
